42fbb4a051e24cc7a06c2d87bbb983d890aeb439
mahdipratama <mahdipratama29@gmail.com>
2022-12-26 13:56:27 +0700

    add input field text animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,10 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
+// document.querySelectorAll('.text__input').forEach((element) => {
+//   element.addEventListener('blur', (event) => {
+//     if (event.target.value !== '') {
+//       const filled = event.target.nextElementSibling.classList.add('filled')
 
-btnHamburger.addEventListener('click', function(){
-  console.log('click hamburger');
-
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
-    });
-    
-  }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-
-  }  
-});
+//     } else {
+//       event.target.nextElementSibling.classList.remove('filled')
+//     }
+//   });
+// });
