218e42e39cdcece8169326b73aab2a8832fe4495
mahdipratama <mahdipratama29@gmail.com>
2023-01-05 19:51:35 +0700

    update form validation when all valid it will print out the Data User

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,7 @@ const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
+
 // Show Error Message
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -20,6 +21,30 @@ function showSuccess(input) {
 }
 
 
+// Check Required field
+function checkRequired(inputArr) {
+  // Loop through the array, then do the check on each one.
+  inputArr.forEach(function (input) {
+    // to Check is input are empty or not
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+
+      // pass a function to check input whether meet certain requirements or not   
+    } else if (checkLength && checkEmail) {
+      checkEmail(email)
+      checkLength(firstName, 5, 15);
+      checkLength(lastName, 5, 15);
+      checkLength(password, 8, 20);
+      // Showing success when all inputs are meet certain requirements
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+
+// console.log(isValid);
+
 // Check email is valid
 function checkEmail(input) {
   const regex =
@@ -30,35 +55,13 @@ function checkEmail(input) {
   } else {
     showError(input, 'Email is not valid')
   }
-}
-
-// Check Required field
-function checkRequired(inputArr) {
-  // Loop through the array, then do the check on each one.
-  inputArr.forEach(function (input) {
-
-    // to Check is input are empty or not
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-
-      // pass a function to check input whether meet certain requirements or not   
-    } else if (checkLength && checkEmail) {
-      checkLength(firstName, 5, 15);
-      checkLength(lastName, 5, 15);
-      checkLength(password, 8, 20);
-      checkEmail(email)
-      // Showing success when all inputs are meet certain requirements
-    } else {
-      showSuccess(input);
-    }
-  });
 };
 
 
 // Get fieldName
 function getFieldName(input) {
   return input.placeholder;
-}
+};
 
 
 // Check input length
@@ -70,13 +73,47 @@ function checkLength(input, min, max) {
   } else {
     showSuccess(input);
   }
-}
+};
+
+// Store Form Data
+function storeFormData() {
+  const user = {
+    firstName: form.firstName.value,
+    lastName: form.lastName.value,
+    email: form.email.value,
+    password: form.password.value,
+  }
+
+  // Do Something with user data
+  console.log(user);
+
+};
+
+
+function processFormData(e) {
+  e.preventDefault();
+
+  // Set isValid to true by default
+  let isValid = true;
+
+  // Check 
+  checkRequired([firstName, lastName, email, password]);
+
+  const inputs = e.target.elements;
+
+  // Loop through the inputs and check if any are invalid
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].parentElement.classList.contains('error')) {
+      isValid = false;
+      break
+    }
+  }
+
+  // if isValid is true, store the form data and print it out
+  if (isValid) storeFormData();
+};
 
 
 // Event listeners
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+form.addEventListener('submit', processFormData)
 
-  // Check each input 
-  checkRequired([firstName, lastName, email, password]);
-});
